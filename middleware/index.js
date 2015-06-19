b72f079c1a064c60ea07a921b96baae6568a9ea5
Umar <umar.hansa@gmail.com>
2015-06-19 17:26:21 +0100

    middleware

---
+++ middleware/index.js
@@ -0,0 +1,34 @@
+
+function Middleware() {
+	this.callbacks = [];
+	this.context = {};
+}
+
+Middleware.prototype.use = function (fn) {
+	this.callbacks.push(fn);
+};
+
+Middleware.prototype.go = function (fn) {
+	var self = this.context;
+	var cbs = this.callbacks;
+
+	function execAllCallbacks(currentCallbacks, lastThing) {
+		return function wrapper() {
+			var currentCb = currentCallbacks.shift();
+			if (typeof currentCb !== 'function') {
+				lastThing.call(self);
+			}
+			function currentDone() {
+				return wrapper(currentCallbacks);
+			}
+			return currentCb.call(self, currentDone);
+		}
+	}
+
+	execAllCallbacks(cbs, function() {
+		fn.call(self);
+	})();
+
+};
+
+module.exports = Middleware;
