2d811a17b82a29aa2a9b4913c41950385586670e
Umar <umar.hansa@gmail.com>
2015-06-23 11:17:21 +0100

    simplify

--- middleware/index.js
+++ middleware/index.js
@@ -10,24 +10,21 @@ Middleware.prototype.use = function (fn) {
 
 Middleware.prototype.go = function (fn) {
 	var self = this.context;
-	var cbs = this.callbacks;
 
-	function execAllCallbacks(currentCallbacks, lastThing) {
-		return function wrapper() {
-			var currentCb = currentCallbacks.shift();
-			if (typeof currentCb !== 'function') {
-				return lastThing.call(self);
-			}
-			function currentDone() {
-				return wrapper(currentCallbacks);
-			}
-			return currentCb.call(self, currentDone);
+	function wrapper(currentCallbacks, lastThing) {
+		var currentCb = currentCallbacks.shift();
+		if (typeof currentCb !== 'function') {
+			return lastThing.call(self);
 		}
+		function currentDone() {
+			return wrapper(currentCallbacks, lastThing);
+		}
+		return currentCb.call(self, currentDone);
 	}
 
-	execAllCallbacks(cbs, function() {
-		fn.call(self);
-	})();
+	wrapper(this.callbacks, function() {
+		fn.call(this);
+	});
 
 };
 
