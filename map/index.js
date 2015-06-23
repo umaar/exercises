6c706455eed5b9553dbbea3c56d5428622485d6b
Umar <umar.hansa@gmail.com>
2015-06-23 12:46:35 +0100

    map

--- map/index.js
+++ map/index.js
@@ -1,22 +1,24 @@
 
-function map(arr, fn) {
+function map(arr, fn, context) {
 	var mappedArr = [];
 	for (var i=0; i<arr.length; i++) {
-		mappedArr.push(fn(arr[i]));
+		var mapFn = fn.bind(context);
+		var result = mapFn(arr[i], i, arr);
+		mappedArr.push(result);
 	}
 	return mappedArr;
 }
 
-var assert = require('assert');
-
-var called = 0;
-var arr = [1, 2, 3];
-
-map(function(value) {
-	called++;
-	return value;
-});
-
-assert.equal(called, 3);
+// var assert = require('assert');
+//
+// var called = 0;
+// var arr = [1, 2, 3];
+//
+// map(function(value) {
+// 	called++;
+// 	return value;
+// });
+//
+// assert.equal(called, 3);
 
 module.exports = map;
