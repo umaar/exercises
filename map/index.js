9ecfb9745c3adfe6b454265a258a3af421754ffe
Umar <umar.hansa@gmail.com>
2015-06-23 12:47:26 +0100

    simplify

--- map/index.js
+++ map/index.js
@@ -2,23 +2,10 @@
 function map(arr, fn, context) {
 	var mappedArr = [];
 	for (var i=0; i<arr.length; i++) {
-		var mapFn = fn.bind(context);
-		var result = mapFn(arr[i], i, arr);
+		var result = fn.call(context, arr[i], i, arr);
 		mappedArr.push(result);
 	}
 	return mappedArr;
 }
-
-// var assert = require('assert');
-//
-// var called = 0;
-// var arr = [1, 2, 3];
-//
-// map(function(value) {
-// 	called++;
-// 	return value;
-// });
-//
-// assert.equal(called, 3);
 
 module.exports = map;
