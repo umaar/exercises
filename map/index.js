03aa561b8340a0181a83eb2f0ab3d405a88f7bb9
Umar <umar.hansa@gmail.com>
2015-06-24 19:00:17 +0100

    babel

--- map/index.js
+++ map/index.js
@@ -1,8 +1,9 @@
 
 function map(arr, fn, context) {
-	var mappedArr = [];
-	for (var i=0; i<arr.length; i++) {
-		var result = fn.call(context, arr[i], i, arr);
+	let mappedArr = [];
+
+	for (let i=0; i<arr.length; i++) {
+		let result = fn.call(context, arr[i], i, arr);
 		mappedArr.push(result);
 	}
 	return mappedArr;
