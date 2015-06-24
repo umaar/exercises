0ea897cd05dda385109e3399d4c58e958731cf62
Umar <umar.hansa@gmail.com>
2015-06-24 21:21:16 +0100

    one liner map

--- map/index.js
+++ map/index.js
@@ -1,2 +1,2 @@
 
-module.exports = (arr, fn, context) => [ for ([i, j] of arr.entries()) fn.call(context, j, i, arr) ]
+module.exports = (arr, fn, context) => [for ([i, j] of arr.entries()) fn.call(context, j, i, arr)]
