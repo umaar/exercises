ffbb07e228d122a82de934f166edf609e780bf2a
Umar <umar.hansa@gmail.com>
2015-06-25 19:04:17 +0100

    classy

--- middleware/index.js
+++ middleware/index.js
@@ -5,27 +5,25 @@ class Middleware {
 		this.middlewareContext = {};
 	}
 
-	use (fn) {
+	use(fn) {
 		this.callbacks.push(fn);
 	}
-}
 
-Middleware.prototype.go = function (fn) {
-	function wrapper(currentCallbacks, completeCallback) {
-		var currentCb = currentCallbacks.shift();
+	go(fn) {
+		function wrapper(currentCallbacks, completeCallback) {
+			var currentCb = currentCallbacks.shift();
 
-		if (typeof currentCb !== 'function') {
-			return completeCallback.call(this);
+			if (typeof currentCb !== 'function') {
+				return completeCallback.call(this);
+			}
+
+			return currentCb.call(this, () => wrapper.call(this, currentCallbacks, completeCallback));
 		}
 
-		return currentCb.call(this, function() {
-			return wrapper.call(this, currentCallbacks, completeCallback);
-		}.bind(this));
+		wrapper.call(this.middlewareContext, this.callbacks, function() {
+			fn.call(this);
+		});
 	}
-
-	wrapper.call(this.middlewareContext, this.callbacks, function() {
-		fn.call(this);
-	});
-};
+}
 
 export default Middleware;
