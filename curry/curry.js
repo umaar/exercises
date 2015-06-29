78cb5fd26e2679ac71a9b9dfff76447680a3a0fd
Umar <umar.hansa@gmail.com>
2015-06-29 23:35:02 +0100

    curry clean

--- curry/curry.js
+++ curry/curry.js
@@ -1,15 +1,13 @@
-// nodemon curry.js --exec "../node_modules/mocha/bin/mocha test.js"
-
 function curry(fn) {
-  return (function wrapper(arr) {
-    return function curried() {
-      var totalArguments = arr.concat(Array.prototype.slice.call(arguments));
-      if (totalArguments.length === fn.length) {
-        return fn.apply(null, totalArguments);
-      }
-      return wrapper(totalArguments);
-    };
-  }([]));
+	return (function wrapper(arr) {
+		return function curried(...args) {
+			var totalArguments = arr.concat(args);
+			if (totalArguments.length === fn.length) {
+				return fn.apply(null, totalArguments);
+			}
+			return wrapper(totalArguments);
+		};
+	}([]));
 }
 
 module.exports = curry;
